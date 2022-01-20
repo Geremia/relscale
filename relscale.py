3c8043b0c8a859b234ce6eb27793b6a00797dfce
Geremia <Geremia@users.noreply.github.com>
2022-01-20 14:04:45 -0700

    initial commit ☧ gratias

---
+++ relscale.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# Automatically scale text up as large as possible to fit within certain page count
+# https://tex.stackexchange.com/q/630429/36603
+
+import os, sys
+
+if len(sys.argv) != 3:
+    print("Usage example: " + sys.argv[0] + " in.tex max_num_pages")
+    exit()
+
+texinput = sys.argv[1]
+
+s_prev = 0; s = 1 #previous and current scale factor
+Δs = 0.001 # 0.1% tolerance
+f = 2 # ∈ (1,∞)
+p_max = int(sys.argv[2]) # max number of pages
+
+while True:
+    os.system('sed -i \'s/\\\\relscale{[0-9.]\+}/\\\\relscale{' + str(s) + '}/\' "' + texinput + '"')
+    p = int(os.popen('xelatex "'+ texinput + "\" | grep -o '([0-9]\+ pages)' | grep -o '[0-9]\+'").read().strip())
+    print("relscale factor = ", s, ",\tpages = ", p)
+    if abs(s - s_prev) < Δs and p <= p_max: # break when scale factor s within Δs tolerance
+        break
+    s_prev = s
+    if p <= p_max:
+        s *= f
+    elif p > p_max:
+        s /= f
+    f = (1+f)/2
